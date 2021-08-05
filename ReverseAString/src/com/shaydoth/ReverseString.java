167f80b07594b7ffa01c561a25adf60c7fcc1ef6
Shaydoth <rhysduggan5@gmail.com>
2021-08-05 12:03:03 +0100

    Added comments

--- ReverseAString/src/com/shaydoth/ReverseString.java
+++ ReverseAString/src/com/shaydoth/ReverseString.java
@@ -4,6 +4,11 @@ import java.util.Arrays;
 
 public class ReverseString {
 
+    /**
+     * This is the entry point of the program
+     *
+     * @param args The arguments of the program
+     */
     public static void main (String[] args) {
 
         if (args.length != 1) {
@@ -19,6 +24,12 @@ public class ReverseString {
         System.out.printf("The reverse of %s is %s", args[0], reverseString(args[0]));
     }
 
+    /**
+     * Method to reverse a string
+     *
+     * @param val the string to be reversed
+     * @return returns the string reversed
+     */
     public static String reverseString(String val) {
         if (val.equals("")) return "";
 
